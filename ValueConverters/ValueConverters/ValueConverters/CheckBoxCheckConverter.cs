ec7874e3ba51ccd64b6f14fcc62d94baaecb3393
LeoMingGit <52078260+LeoMingGit@users.noreply.github.com>
2019-06-23 19:22:52 +0800

    Add files via upload

---
+++ ValueConverters/ValueConverters/ValueConverters/CheckBoxCheckConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ValueConverters
+{
+    class CheckBoxCheckConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value.ToString().ToUpper() == "MARRIED")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool married = System.Convert.ToBoolean(value);
+            if (married == true)
+                return "Married";
+            else
+                return "Unmarried";
+        }
+    }
+}
